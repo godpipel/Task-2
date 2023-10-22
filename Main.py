6109f8af9c45db69835f628ac544b13fc287259e
goodpipel17 <20082010085@student.upnjatim.ac.id>
2023-10-22 19:16:25 +0700

    Changes Math

--- Main.py
+++ Main.py
@@ -1,17 +1,17 @@
-from math_function import add
+
+from Math_function import add
 
 
 def main():
-
     data_1 = int(input("masukkan input 1 :"))
     data_2 = int(input("masukkan input 2 :"))
     operator = input("masukkan operator :")
 
-    if operator == "+":
+    if operator == "+" or operator == "-" or operator == "%" :
         result = add(data_1, data_2)
-
-    print("{} {} {} = {} ".format(data_1, operator, data_2, result))
-
+        print("{} {} {} = {} ".format(data_1, operator, data_2, result))
+    else:
+        print("Operator tidak valid. Gunakan operator '+,-,(%)' untuk penambahan,pengurangan dan modulus.")
 
 if __name__ == "__main__":
     print("Hello Main !")
